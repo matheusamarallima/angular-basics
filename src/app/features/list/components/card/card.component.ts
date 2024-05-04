2291216cf038df201697fb84893d066b1c8cd81a
Mathe <matheusamrl@gmail.com>
2024-05-04 17:29:45 -0300

    card component created and updated

---
+++ src/app/features/list/components/card/card.component.ts
@@ -0,0 +1,23 @@
+import {Component, computed, Input, input, signal} from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {MatCard, MatCardActions, MatCardHeader, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
+import {Product} from "../../../../shared/interfaces/product.interface";
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [
+    MatButton,
+    MatCard,
+    MatCardActions,
+    MatCardHeader,
+    MatCardSubtitle,
+    MatCardTitle
+  ],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+  product = input.required<Product>();
+  productTitle = computed(()=> this.product().title)
+}
