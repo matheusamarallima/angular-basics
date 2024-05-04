f7f59a41860f2ab6b55e97097d24680077d7bff0
Mathe <matheusamrl@gmail.com>
2024-05-04 17:09:10 -0300

    list now has ang material

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,11 +1,13 @@
 import {Component, inject} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {ProductsService} from "../../shared/services/products.service";
+import {MatCardModule} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButton],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
