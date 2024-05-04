a428d6768facc3602fa03956fe572bdad3100fec
Mathe <matheusamrl@gmail.com>
2024-05-04 16:37:00 -0300

    listing working

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-list',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-
+  httpClient = inject(HttpClient);
+  products: any[] = [];
+  ngOnInit(){
+    this.httpClient.get<any>("/api/products").subscribe((products) => {
+      this.products = products;
+    })
+  }
 }
