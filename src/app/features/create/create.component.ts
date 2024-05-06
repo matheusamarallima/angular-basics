133fbf071cb178a0f129ab34a77e2595ccee04bb
Mathe <matheusamrl@gmail.com>
2024-05-06 16:07:25 -0300

    create component with button to save form info.

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInput
+  ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+  form = new FormGroup({
+    title: new FormControl<string>('', {
+      nonNullable : true,
+      validators: Validators.required,
+    })
+  });
+
+  onSubmit(){
+    this.form.controls.title.value;
+  }
 
 }
