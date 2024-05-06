eb218425478421ba80d0b57ed9bd2ecd679be797
Mathe <matheusamrl@gmail.com>
2024-05-06 15:50:40 -0300

    routing done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import {ListComponent} from "./features/list/list.component";
+import {createComponent} from "@angular/core";
+import {CreateComponent} from "./features/create/create.component";
 
-export const routes: Routes = [{
+export const routes: Routes = [
+  {
   path: '',
   component: ListComponent
-}];
+},
+  {
+    path: 'create-product',
+    component: CreateComponent
+  }
+];
