bbddf7bf24e7b6988ca418ec41a4e36c4b4e4a3a
Mathe <matheusamrl@gmail.com>
2024-05-06 15:55:03 -0300

    Lazy loading on create component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,6 @@ export const routes: Routes = [
 },
   {
     path: 'create-product',
-    component: CreateComponent
+    loadComponent: ()=> import('./features/create/create.component').then(m => m.CreateComponent),
   }
 ];
